60947a916a28ab83e2752250809d112e1f33f8e0
YcleptJohn <john.taylor@holidayextras.com>
2018-01-18 22:53:54 +0000

    Setup config.js ready to handle environmental configs

---
+++ config.js
@@ -0,0 +1,17 @@
+const ENV = (process.env.NODE_ENV || 'development')
+
+let configs = {}
+
+configs['default'] = {
+  SERVER_PORT: 8080 
+}
+
+configs['development'] = {
+  SERVER_PORT: 5000 
+}
+
+configs['staging']= {}
+
+configs['production'] = {}
+
+module.exports = Object.assign({}, configs.default, configs[ENV])
