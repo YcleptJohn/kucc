3c07570ccc033556ccf1d605ce150bf438c20fca
YcleptJohn <john.taylor@holidayextras.com>
2018-01-19 01:50:30 +0000

    Install and demo semantic (non-themable)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 import { Link, Route, Switch } from 'react-router-dom'
 import ReactDOM from 'react-dom'
+import { Button } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return ( 
       <div>
-        <Link to='/'>Home</Link>
-        { ' | ' }
-        <Link to='/about'>About</Link>
-    
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/about' component={About}/>
-      </Switch>
-    </div>
+        <Link to='/'>
+          <Button primary>
+            Home
+          </Button>
+        </Link>
+
+        <Link to='/about'>
+          <Button>
+            About
+          </Button>
+        </Link>
+
+        <Switch>
+          <Route exact path='/' component={Home}/>
+         <Route path='/about' component={About}/>
+        </Switch>
+      </div>
     )
   }
 }
