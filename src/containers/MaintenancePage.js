d9215a9ccb3cdafbbc1429a37ba140722bef3538
YcleptJohn <Ycleptjohn@gmail.com>
2018-02-27 00:31:24 +0000

    Force build on start?

--- src/containers/MaintenancePage.js
+++ src/containers/MaintenancePage.js
@@ -10,8 +10,8 @@ class MaintenancePage extends Component {
         page 
       >
         <Header as='h2' icon inverted>
-          KUCC's newish website is coming soon!
-          <Header.Subheader>In the meantime please keep up to date with the club on our facebook page below.</Header.Subheader>
+          KUCC's new website is coming soon!
+          <Header.Subheader>In the meantime please keep up to date with the club on our facebook page below</Header.Subheader>
         </Header>
         <Divider horizontal/>
         <a href="https://www.facebook.com/groups/363156430381405/">
