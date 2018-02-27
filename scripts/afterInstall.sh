339483f22d154f8c04e5b927f6807e3ab66f9430
YcleptJohn <Ycleptjohn@gmail.com>
2018-02-27 00:23:30 +0000

    FORCE NO NPM TO SEE IF THE DEPLOY WORKS

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,9 +1,1 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-nvm i
-nvm use
-npm i --global gulp-cli
-npm install
-npm run build:all
+echo "fuck my life"
