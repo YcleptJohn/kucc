ef971ec2fe649a2ddbe3dd84338f6b63ddc7f5e9
YcleptJohn <Ycleptjohn@gmail.com>
2018-03-05 00:29:04 +0000

    Run npm start independent with no hangup

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ cd ~/node
 curl --silent --location https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash -
 . ~/.bashrc
 nvm install
-npm start & echo "Application started - JJT"
+nohup npm start &
