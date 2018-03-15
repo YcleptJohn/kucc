db00be0594de674eefde95f665ffd6e33b3a2559
YcleptJohn <Ycleptjohn@gmail.com>
2018-03-15 00:59:57 +0000

    Prove github deployment

--- src/containers/MaintenancePage.js
+++ src/containers/MaintenancePage.js
@@ -11,7 +11,7 @@ class MaintenancePage extends Component {
       >
         <Header as='h2' icon inverted>
           KUCC's new website is coming very soon!
-          <Header.Subheader>In the meantime please keep up to date with the club on our facebook page below</Header.Subheader>
+          <Header.Subheader>In the meantime please keep up to date with the club on our facebook page below!</Header.Subheader>
         </Header>
         <Divider horizontal/>
         <a href="https://www.facebook.com/groups/363156430381405/">
