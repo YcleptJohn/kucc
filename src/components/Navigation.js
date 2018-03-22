0192a15b942f9695e5ac34025184ee2b7e7450b2
YcleptJohn <Ycleptjohn@gmail.com>
2018-03-22 23:43:10 +0000

    Slight improvements - Actual responsive separation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Container, Menu, Segment, Image, Responsive, Sidebar, Icon } from 'semantic-ui-react'
+import { Button, Container, Menu, Segment, Image, Responsive, Sidebar, Icon, Divider } from 'semantic-ui-react'
 import { Link, Redirect } from 'react-router-dom'
 
 class Navigation extends Component {
@@ -32,10 +32,33 @@ class Navigation extends Component {
         let activeItem = this.state.activeItem
         return (
             <div>
-                <Responsive as={Segment} minWidth={800} inverted fixed='top'>
-                    <Container>
-                        <Menu inverted pointing secondary>
-                            <Image as={Link} to='/' className='nav-logo' src='https://i.imgur.com/prbTzQs.png' onClick={this.handleLogoClick} />
+                <Responsive minWidth={800}>
+                    <Segment basic inverted fixed='top'>
+                            <Container>
+                                <Menu inverted pointing secondary>
+                                    <Image as={Link} to='/' className='nav-logo' src='https://i.imgur.com/prbTzQs.png' onClick={this.handleLogoClick} />
+                                    <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+                                    <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
+                                    <Menu.Item as={Link} to='/trips' name='trips' active={activeItem === 'trips'} onClick={this.handleItemClick} />
+                                    <Menu.Menu position='right'>
+                                        <Button.Group>
+                                            <Button inverted>Sign  Up</Button>
+                                            <Button.Or />
+                                            <Button inverted color='yellow'>Log In</Button>
+                                        </Button.Group>
+                                    </Menu.Menu>
+                                </Menu>
+                            </Container>
+                    </Segment>
+                    {this.props.children}
+                </Responsive>
+                <Responsive as={Segment} basic maxWidth={799} inverted fixed='top'>
+                    <Image className='nav-logo-mobile' size='tiny' floated='left' src='https://i.imgur.com/prbTzQs.png'/>
+                    <Button icon='sidebar' floated='right' onClick={this.toggleMobileSidebar}/><br/><br/>
+                </Responsive>
+                <Responsive maxWidth={799}>
+                    <Sidebar.Pushable as={Segment}>
+                        <Sidebar as={Menu} animation='slide along' width='thin' visible={this.state.showMobileSidebar} icon='labeled' vertical inverted>
                             <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
                             <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
                             <Menu.Item as={Link} to='/trips' name='trips' active={activeItem === 'trips'} onClick={this.handleItemClick} />
@@ -46,23 +69,12 @@ class Navigation extends Component {
                                     <Button inverted color='yellow'>Log In</Button>
                                 </Button.Group>
                             </Menu.Menu>
-                        </Menu>
-                    </Container>
+                        </Sidebar>
+                        <Sidebar.Pusher>
+                            {this.state.children}
+                        </Sidebar.Pusher>
+                    </Sidebar.Pushable>
                 </Responsive>
-                <Responsive as={Segment} basic maxWidth={799} inverted fixed='top'>
-                    <Image className='nav-logo-mobile' size='tiny' floated='left' src='https://i.imgur.com/prbTzQs.png'/>
-                    <Button icon='sidebar' floated='right' onClick={this.toggleMobileSidebar}/><br/><br/>
-                </Responsive>
-                <Sidebar.Pushable as={Segment}>
-                    <Sidebar as={Menu} animation='slide along' width='thin' visible={this.state.showMobileSidebar} icon='labeled' vertical inverted>
-                        <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-                        <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
-                        <Menu.Item as={Link} to='/trips' name='trips' active={activeItem === 'trips'} onClick={this.handleItemClick} />
-                    </Sidebar>
-                    <Sidebar.Pusher>
-                        {this.state.children}
-                    </Sidebar.Pusher>
-                </Sidebar.Pushable>
             </div>
         )
     }
