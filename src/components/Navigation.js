ba8ddfc7438293d250e48e9295e903dc73a67e47
YcleptJohn <Ycleptjohn@gmail.com>
2018-03-28 22:39:25 +0100

    Move menu markup for a function - could be json loopable?

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -29,10 +29,27 @@ class Navigation extends Component {
     this.setState({showMobileSidebar: !this.state.showMobileSidebar})
   }
 
-  getMenuMarkup () {
+  getMenuMarkup (platform) {
+    if (platform === 'mobile') {
+      return (
+        <Sidebar as={Menu} animation='slide along' width='thin' visible={this.state.showMobileSidebar} icon='labeled' vertical inverted>
+          <Menu.Item as={Link} to='/' name='home' active={this.state.activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item name='about' active={this.state.activeItem === 'about'} onClick={this.handleItemClick} />
+          <Menu.Item as={Link} to='/trips' name='trips' active={this.state.activeItem === 'trips'} onClick={this.handleItemClick} />
+          <Divider inverted />
+          <Menu.Menu position='right'>
+            <Button.Group>
+              <Button inverted>Sign  Up</Button>
+              <Button.Or />
+              <Button inverted color='yellow'>Log In</Button>
+            </Button.Group>
+          </Menu.Menu>
+        </Sidebar>
+      )
+    }
     return (
       <Menu inverted pointing secondary>
-        <Image as={Link} to='/' className='nav-logo' src='https://i.imgur.com/prbTzQs.png' onClick={this.handleLogoClick} />
+        <Image as={Link} to='/' className='nav-logo' src='img/logo.png' onClick={this.handleLogoClick} />
         <Menu.Item as={Link} to='/' name='home' active={this.state.activeItem === 'home'} onClick={this.handleItemClick} />
         <Menu.Item name='about' active={this.state.activeItem === 'about'} onClick={this.handleItemClick} />
         <Menu.Item as={Link} to='/trips' name='trips' active={this.state.activeItem === 'trips'} onClick={this.handleItemClick} />
@@ -48,36 +65,23 @@ class Navigation extends Component {
   }
 
   render () {
-    let activeItem = this.state.activeItem
     return (
       <div>
         <Responsive minWidth={800}>
           <Segment basic inverted fixed='top'>
             <Container>
-              {this.getMenuMarkup()}
+              {this.getMenuMarkup('desktop')}
             </Container>
           </Segment>
           {this.props.children}
         </Responsive>
         <Responsive as={Segment} basic maxWidth={799} inverted fixed='top'>
-          <Image className='nav-logo-mobile' size='tiny' floated='left' src='https://i.imgur.com/prbTzQs.png' />
+          <Image className='nav-logo-mobile' size='tiny' floated='left' src='img/logo.png' />
           <Button icon='sidebar' floated='right' onClick={this.toggleMobileSidebar} /><br /><br />
         </Responsive>
         <Responsive maxWidth={799}>
           <Sidebar.Pushable as={Segment}>
-            <Sidebar as={Menu} animation='slide along' width='thin' visible={this.state.showMobileSidebar} icon='labeled' vertical inverted>
-              <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-              <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
-              <Menu.Item as={Link} to='/trips' name='trips' active={activeItem === 'trips'} onClick={this.handleItemClick} />
-              <Divider inverted />
-              <Menu.Menu position='right'>
-                <Button.Group>
-                  <Button inverted>Sign  Up</Button>
-                  <Button.Or />
-                  <Button inverted color='yellow'>Log In</Button>
-                </Button.Group>
-              </Menu.Menu>
-            </Sidebar>
+            {this.getMenuMarkup('mobile')}
             <Sidebar.Pusher>
               {this.state.children}
             </Sidebar.Pusher>
