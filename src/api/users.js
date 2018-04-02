3af57a012581d58119677ce109802832888adbe3
YcleptJohn <Ycleptjohn@gmail.com>
2018-04-03 00:29:29 +0100

    Hash+salted account creation + login verification

--- src/api/users.js
+++ src/api/users.js
@@ -1,10 +1,44 @@
 const app = require('../server.js')
 const database = require('../lib/database.js')
 const validation = require('../lib/validation.js')
+const auth = require('../lib/auth.js')
 
-app.post('/api/user/create', (req, res) => {
-  if (!validation.hasProperties(req.body, ['firstname', 'surname'])) return res.send(400)
-  return res.send('Hello ' + req.body.firstname)
+app.post('/api/user/create', async (req, res) => {
+  if (!validation.hasTruthyProperties(req.body, ['forename', 'surname', 'email', 'password'])) return res.status(400).send('Missing params')
+  if (!validation.checkEmailFormat(req.body.email)) return res.status(400).send('Invalid email')
+  const salt = auth.generateSalt()
+  const hashedPassword = auth.hashPassword(req.body.password, salt)
+  try {
+    await database.query(`
+      INSERT INTO 
+        kucc.users (forename, surname, kentId, email, password, salt) 
+      VALUES (?, ?, ?, ?, ?, ?)`,
+    [
+      req.body.forename,
+      req.body.surname,
+      (req.body.kentId || null),
+      req.body.email,
+      hashedPassword,
+      salt
+    ]
+    )
+    return res.status(200).send('OK')
+  } catch (e) {
+    if (e.code === 'ER_DUP_ENTRY') return res.status(409).send('This account already exists.')
+    return res.status(500).send('Database error')
+  }
+})
+
+app.post('/api/user/login', async (req, res) => {
+  if (!validation.hasTruthyProperties(req.body, ['email', 'password'])) return res.status(400).send('Missing params')
+  if (!validation.checkEmailFormat(req.body.email)) return res.status(400).send('Invalid email')
+  let success = false
+  try {
+    success = await auth.verifyLogin(req.body.email, req.body.password)
+    return res.send(success)
+  } catch (e) {
+    return res.status(500).send(e.message)
+  }
 })
 
 app.get('/api/testdb', async (req, res) => {
---
+++ src/lib/auth.js
@@ -0,0 +1,28 @@
+const auth = module.exports = {}
+const crypto = require('crypto')
+const database = require('../lib/database.js')
+const util = require('util')
+
+auth.generateSalt = () => {
+  return crypto.randomBytes(512).toString('hex')
+}
+
+auth.hashPassword = (password, salt) => {
+  salt = salt || auth.generateSalt() // If salt wasn't specified - make one (account creation)
+  return crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512').toString('hex')
+}
+
+auth.verifyLogin = util.promisify(async (email, password, cb) => {
+  let account
+  try {
+    const rows = await database.query('SELECT * FROM kucc.users WHERE email = ?', [email])
+    if (!rows.length || rows.length === 0) return cb(new Error('Account not found'), null)
+    account = rows[0]
+  } catch (e) {
+    return cb(e, null)
+  }
+
+  const attemptedHash = auth.hashPassword(password, account.salt)
+  if (attemptedHash !== account.password) return cb(null, false)
+  return cb(null, true)
+})
