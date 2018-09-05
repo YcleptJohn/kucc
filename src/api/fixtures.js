07300097434b16b57acec1b9634dce7eb8b08b66
YcleptJohn <john.taylor@holidayextras.com>
2018-09-05 12:38:33 +0200

    Add api method for fetching trips by id - probably needs better errors

--- src/api/fixtures.js
+++ src/api/fixtures.js
@@ -13,3 +13,18 @@ app.get('/api/fixtures/getAll', async (req, res) => {
   }
   return res.status(200).send(r)
 })
+
+app.get('/api/fixtures/get/:tripId', async (req, res) => {
+  if (!req.params) return res.status(400).send('Missing params')
+  let r
+  try {
+    r = await database.query(`
+      SELECT * FROM kucc.fixtures WHERE fixtureId = ?
+    `, [
+      req.params.tripId
+    ])
+  } catch (e) {
+    return res.status(500).send('An error occurred communicating with the database.')
+  }
+  return res.status(200).send(r)
+})
